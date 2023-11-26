e05e1346fa414a6614c349915393f9853c4e1f9b
Alan Fernandez <alan.f@msn.com>
2023-11-25 23:31:59 -0300

    fix dev webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,13 @@
-import { merge } from 'webpack-merge';
-import common from './webpack.common.js';
-import { join } from 'path';
-export default merge(common, {
-  mode: 'development', // Don't minify the source
-  devtool: 'eval-source-map', // Source map for easier development
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-source-map',
   devServer: {
     static: {
-      directory: join(__dirname, './dist'), // Serve static files from here
+      directory: path.join(__dirname, './dist'), // Serve static files from here
     },
     hot: true,
   },
 });
-
