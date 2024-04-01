b1bc6bd4b484183ae3a92f39fc203db0b8677a94
Alan Fernandez <alan.f@msn.com>
2024-04-01 20:11:02 -0300

    added assets ids to mesh data

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -11,22 +11,26 @@ const assets: AssetCreators = {
   grass: (x: number, y: number) => {
     const material = new THREE.MeshLambertMaterial({ color: 0x00aa00 });
     const mesh = new THREE.Mesh(geometry, material);
+    mesh.userData = { id: 'grass' };
     mesh.position.set(x, -0.5, y);
     return mesh;
   },
   'building-1': (x: number, y: number, height: number) => {
     const mesh = new THREE.Mesh(geometry, buildingMaterial);
+    mesh.userData = { id: 'building-1' };
     mesh.position.set(x, 0.5, y);
     return mesh;
   },
   'building-2': (x: number, y: number) => {
     const mesh = new THREE.Mesh(geometry, buildingMaterial);
+    mesh.userData = { id: 'building-2' };
     mesh.scale.set(1, 2, 1);
     mesh.position.set(x, 1, y);
     return mesh;
   },
   'building-3': (x: number, y: number) => {
     const mesh = new THREE.Mesh(geometry, buildingMaterial);
+    mesh.userData = { id: 'building-3' };
     mesh.scale.set(1, 3, 1);
     mesh.position.set(x, 1.5, y);
     return mesh;
