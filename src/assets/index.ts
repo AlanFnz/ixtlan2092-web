8a2b0e66632493fd2e597115ad5e6d5f361788f9
Alan Fernandez <alan.f@msn.com>
2024-04-02 21:14:15 -0300

    storing coordinates on mesh data

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -17,21 +17,21 @@ const assets: AssetCreators = {
   [ASSET_ID.GRASS]: (x: number, y: number) => {
     const material = new THREE.MeshLambertMaterial({ color: 0x00aa00 });
     const mesh = new THREE.Mesh(geometry, material);
-    mesh.userData = { id: ASSET_ID.GRASS };
+    mesh.userData = { id: ASSET_ID.GRASS, x, y };
     mesh.position.set(x, -0.5, y);
     return mesh;
   },
   [ASSET_ID.BUILDING_1]: (x: number, y: number, height: number) => {
     const material = new THREE.MeshLambertMaterial({ color: 0x777777 });
     const mesh = new THREE.Mesh(geometry, material);
-    mesh.userData = { id: ASSET_ID.BUILDING_1 };
+    mesh.userData = { id: ASSET_ID.BUILDING_1, x, y };
     mesh.position.set(x, 0.5, y);
     return mesh;
   },
   [ASSET_ID.BUILDING_2]: (x: number, y: number) => {
     const material = new THREE.MeshLambertMaterial({ color: 0x777777 });
     const mesh = new THREE.Mesh(geometry, material);
-    mesh.userData = { id: ASSET_ID.BUILDING_2 };
+    mesh.userData = { id: ASSET_ID.BUILDING_2, x, y };
     mesh.scale.set(1, 2, 1);
     mesh.position.set(x, 1, y);
     return mesh;
@@ -39,7 +39,7 @@ const assets: AssetCreators = {
   [ASSET_ID.BUILDING_3]: (x: number, y: number) => {
     const material = new THREE.MeshLambertMaterial({ color: 0x777777 });
     const mesh = new THREE.Mesh(geometry, material);
-    mesh.userData = { id: ASSET_ID.BUILDING_3 };
+    mesh.userData = { id: ASSET_ID.BUILDING_3, x, y };
     mesh.scale.set(1, 3, 1);
     mesh.position.set(x, 1.5, y);
     return mesh;
