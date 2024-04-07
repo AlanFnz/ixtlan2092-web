1ed1e2d6a01117d80d0a3f20f8f94ad1841a2027
Alan Fernandez <alan.f@msn.com>
2024-04-07 19:59:10 -0300

    deselect tool on click if already selected

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,22 @@ let selectControl: HTMLButtonElement | null;
 
 window.onload = () => {
   window.game = createGame();
-  window.setActiveTool = (event: any, toolId: string) => {
-    if (selectControl) selectControl.classList.remove('selected');
-    selectControl = event.target;
-    selectControl?.classList.add('selected');
-
-    window.game.setActiveToolId(toolId);
+  window.setActiveTool = (event, toolId) => {
+    if (selectControl === event.target) {
+      selectControl?.classList.toggle('selected');
+      
+      if (!selectControl?.classList.contains('selected')) {
+        window.game.setActiveToolId('');
+      } else {
+        window.game.setActiveToolId(toolId);
+      }
+    } else {
+      if (selectControl) selectControl.classList.remove('selected');
+      selectControl = event.target;
+      selectControl?.classList.add('selected');
+      
+      window.game.setActiveToolId(toolId);
+    }
   };
 };
 
