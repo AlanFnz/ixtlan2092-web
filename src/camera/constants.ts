6ea1dc9b3ad15e3e3161c6d37cb2ee6f59479829
Alan Fernandez <alan.f@msn.com>
2024-04-12 18:29:26 -0300

    small camera config adjustments

--- src/camera/constants.ts
+++ src/camera/constants.ts
@@ -7,15 +7,15 @@ export const MIDDLE_MOUSE_BUTTON = 1;
 export const RIGHT_MOUSE_BUTTON = 2;
 
 export const MIN_CAMERA_RADIUS = 10;
-export const MAX_CAMERA_RADIUS = 20;
+export const MAX_CAMERA_RADIUS = 50;
 export const INIT_CAMERA_ELEVATION = 45;
-export const MIN_CAMERA_ELEVATION = 30;
+export const MIN_CAMERA_ELEVATION = 10;
 export const MAX_CAMERA_ELEVATION = 90;
 export const INIT_CAMERA_AZIMUTH = 135;
 
 export const ROTATION_SENSITIVITY = 0.5;
 export const ZOOM_SENSITIVITY = 0.02;
-export const PAN_SENSITIVITY = -0.01;
+export const PAN_SENSITIVITY = -0.03;
 
 export const Y_AXIS = new THREE.Vector3(0, 1, 0);
 
