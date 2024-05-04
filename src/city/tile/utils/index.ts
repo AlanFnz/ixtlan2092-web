f344c95e4b2004db7737accf84b5963e36de01b7
Alan Fernandez <alan.f@msn.com>
2024-05-04 14:11:50 -0300

    find tile by coordinates util

---
+++ src/city/tile/utils/index.ts
@@ -0,0 +1,21 @@
+import { Coordinate, Tile } from '../../constants';
+
+function findTileByCoordinates(
+  tiles: Tile[][],
+  coordinate: Coordinate
+): Tile | undefined {
+  // tiles is a 2D array
+  if (
+    coordinate.x < 0 ||
+    coordinate.y < 0 ||
+    coordinate.x >= tiles.length ||
+    coordinate.y >= tiles[0].length
+  ) {
+    return undefined; // ensure the coordinates are within the bounds of the array.
+  }
+
+  return tiles[coordinate.x][coordinate.y];
+}
+
+export { findTileByCoordinates };
+
