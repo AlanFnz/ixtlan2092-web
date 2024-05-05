21b8d8136abde0a7bf0ba601e1538375fd31b1ae
Alan Fernandez <alan.f@msn.com>
2024-05-05 15:43:55 -0300

    init config file

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+export default {
+  CITIZEN: {
+    MIN_WORKING_AGE: 16,
+    RETIREMENT_AGE: 65,
+    MAX_JOB_SEARCH_DISTANCE: 4,
+  },
+  ZONE: {
+    ABANDONMENT_THRESHOLD: 10, // number of days before abandonment
+    ABANDONMENT_CHANCE: 0.25,
+    DEVELOPMENT_CHANCE: 0.25,
+    MAX_ROAD_SEARCH_DISTANCE: 3, // max distance between buildng and road
+    MAX_RESIDENTS: 4,
+    RESIDENT_MOVE_IN_CHANCE: 0.5,
+  },
+};
+
