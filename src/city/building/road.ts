53b9b8049d4e7319b5a503778e2740f942960658
Alan Fernandez <alan.f@msn.com>
2024-05-22 20:15:40 -0300

    updated road class

--- src/city/building/road.ts
+++ src/city/building/road.ts
@@ -1,12 +1,100 @@
+import { ICity } from '..';
 import { Building } from './building';
-import { BUILDING_TYPE } from './constants';
+import { BUILDING_TYPE, BuildingType } from './constants';
 
 export interface IRoad {}
 
 export class Road extends Building implements IRoad {
+  style: string;
+  hideTerrain: boolean;
+
   constructor(x: number, y: number) {
     super(x, y);
+    this.name = 'Two-Lane Road';
     this.type = BUILDING_TYPE.ROAD;
+    this.style = 'straight';
+    this.hideTerrain = true;
+  }
+
+  refresh(city: ICity): void {
+    // check which adjacent tiles are roads
+    const top =
+      city.getTile(this.x, this.y + 1)?.building?.type === this.type ?? false;
+    const bottom =
+      city.getTile(this.x, this.y - 1)?.building?.type === this.type ?? false;
+    const left =
+      city.getTile(this.x - 1, this.y)?.building?.type === this.type ?? false;
+    const right =
+      city.getTile(this.x + 1, this.y)?.building?.type === this.type ?? false;
+
+    // check all combinations
+    // four-way intersection
+    if (top && bottom && left && right) {
+      this.style = 'intersection';
+      this.rotation = 0;
+      // T intersection
+    } else if (!top && bottom && left && right) {
+      // bottom-left-right
+      this.style = 'tee';
+      this.rotation = 180;
+    } else if (top && !bottom && left && right) {
+      // top-left-right
+      this.style = 'tee';
+      this.rotation = 0;
+    } else if (top && bottom && !left && right) {
+      // top-bottom-right
+      this.style = 'tee';
+      this.rotation = 90;
+    } else if (top && bottom && left && !right) {
+      // top-bottom-left
+      this.style = 'tee';
+      this.rotation = 270;
+      // Corner
+    } else if (top && !bottom && left && !right) {
+      // top-left
+      this.style = 'corner';
+      this.rotation = 270;
+    } else if (top && !bottom && !left && right) {
+      // top-right
+      this.style = 'corner';
+      this.rotation = 0;
+    } else if (!top && bottom && left && !right) {
+      // bottom-left
+      this.style = 'corner';
+      this.rotation = 180;
+    } else if (!top && bottom && !left && right) {
+      // bottom-right
+      this.style = 'corner';
+      this.rotation = 90;
+      // Straight
+    } else if (top && bottom && !left && !right) {
+      // top-bottom
+      this.style = 'straight';
+      this.rotation = 0;
+    } else if (!top && !bottom && left && right) {
+      // left-right
+      this.style = 'straight';
+      this.rotation = 90;
+      // Dead end
+    } else if (top && !bottom && !left && !right) {
+      // top
+      this.style = 'end';
+      this.rotation = 0;
+    } else if (!top && bottom && !left && !right) {
+      // bottom
+      this.style = 'end';
+      this.rotation = 180;
+    } else if (!top && !bottom && left && !right) {
+      // left
+      this.style = 'end';
+      this.rotation = 270;
+    } else if (!top && !bottom && !left && right) {
+      // right
+      this.style = 'end';
+      this.rotation = 90;
+    }
+
+    this.isMeshOutOfDate = true;
   }
 }
 
