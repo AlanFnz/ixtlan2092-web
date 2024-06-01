44d74f0f15a73a78ce29a9252c9a56573225746e
Alan Fernandez <alan.f@msn.com>
2024-06-01 14:47:46 -0300

    camera manager updates

--- src/cameraManager/constants.ts
+++ src/cameraManager/constants.ts
@@ -8,11 +8,11 @@ export const RIGHT_MOUSE_BUTTON = 2;
 
 export const CAMERA_SIZE = 10;
 export const MIN_CAMERA_RADIUS = 10;
-export const MAX_CAMERA_RADIUS = 50;
+export const MAX_CAMERA_RADIUS = 5;
 export const INIT_CAMERA_ELEVATION = 45;
 export const MIN_CAMERA_ELEVATION = 10;
 export const MAX_CAMERA_ELEVATION = 90;
-export const INIT_CAMERA_AZIMUTH = 135;
+export const INIT_CAMERA_AZIMUTH = 225;
 
 export const AZIMUTH_SENSITIVITY = 0.2;
 export const ELEVATION_SENSITIVITY = 0.02;
