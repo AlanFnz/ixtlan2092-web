0a6c189ef44de0ef35e4e2725868683467ec0446
Alan Fernandez <alan.f@msn.com>
2024-06-08 19:35:52 -0300

    fixed camera parameters #15

--- src/cameraManager/constants.ts
+++ src/cameraManager/constants.ts
@@ -7,8 +7,8 @@ export const MIDDLE_MOUSE_BUTTON = 1;
 export const RIGHT_MOUSE_BUTTON = 2;
 
 export const CAMERA_SIZE = 10;
-export const MIN_CAMERA_RADIUS = 10;
-export const MAX_CAMERA_RADIUS = 5;
+export const MIN_CAMERA_RADIUS = 0.1;
+export const MAX_CAMERA_RADIUS = 1;
 export const INIT_CAMERA_ELEVATION = 45;
 export const MIN_CAMERA_ELEVATION = 10;
 export const MAX_CAMERA_ELEVATION = 90;
@@ -16,7 +16,7 @@ export const INIT_CAMERA_AZIMUTH = 225;
 
 export const AZIMUTH_SENSITIVITY = 0.2;
 export const ELEVATION_SENSITIVITY = 0.02;
-export const ZOOM_SENSITIVITY = 0.02;
+export const ZOOM_SENSITIVITY = 0.002;
 export const PAN_SENSITIVITY = -0.03;
 
 export const Y_AXIS = new THREE.Vector3(0, 1, 0);
