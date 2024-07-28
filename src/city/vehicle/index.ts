cc3fcd64ee3d001a3d196a51f676ee91e8478a0c
Alan Fernandez <alan.f@msn.com>
2024-07-28 13:44:50 +0200

    small refactor

--- src/city/vehicle/index.ts
+++ src/city/vehicle/index.ts
@@ -44,7 +44,7 @@ export class Vehicle extends THREE.Group {
     this.updateWorldPositions();
   }
 
-  getCycleTime(): number {
+  get cycleTime(): number {
     const distance = this.originToDestination.length();
     const cycleDuration = distance / CONFIG.VEHICLE.SPEED;
     const value = (Date.now() - this.cycleStartTime) / cycleDuration;
@@ -52,7 +52,7 @@ export class Vehicle extends THREE.Group {
     return Math.max(0, Math.min(value, 1));
   }
 
-  getAge(): number {
+  get age(): number {
     return Date.now() - this.createdTime;
   }
 
@@ -67,25 +67,22 @@ export class Vehicle extends THREE.Group {
       return;
     }
 
-    if (this.getAge() > CONFIG.VEHICLE.MAX_LIFETIME) {
+    if (this.age > CONFIG.VEHICLE.MAX_LIFETIME) {
       this.dispose();
       return;
     }
 
-    const cycleTime = this.getCycleTime();
-    if (cycleTime === 1) {
+    if (this.cycleTime === 1) {
       this.pickNewDestination();
     } else {
       this.position.copy(this.originWorldPosition);
-      this.position.lerp(this.destinationWorldPosition, cycleTime);
+      this.position.lerp(this.destinationWorldPosition, this.cycleTime);
     }
 
     this.updateOpacity();
   }
 
   updateOpacity(): void {
-    const age = this.getAge();
-
     const setOpacity = (opacity: number) => {
       this.traverse((obj) => {
         if ((obj as THREE.Mesh).material) {
@@ -97,11 +94,11 @@ export class Vehicle extends THREE.Group {
       });
     };
 
-    if (age < CONFIG.VEHICLE.FADE_TIME) {
-      setOpacity(age / CONFIG.VEHICLE.FADE_TIME);
-    } else if (CONFIG.VEHICLE.MAX_LIFETIME - age < CONFIG.VEHICLE.FADE_TIME) {
+    if (this.age < CONFIG.VEHICLE.FADE_TIME) {
+      setOpacity(this.age / CONFIG.VEHICLE.FADE_TIME);
+    } else if (CONFIG.VEHICLE.MAX_LIFETIME - this.age < CONFIG.VEHICLE.FADE_TIME) {
       setOpacity(
-        (CONFIG.VEHICLE.MAX_LIFETIME - age) / CONFIG.VEHICLE.FADE_TIME
+        (CONFIG.VEHICLE.MAX_LIFETIME - this.age) / CONFIG.VEHICLE.FADE_TIME
       );
     } else {
       setOpacity(1);
